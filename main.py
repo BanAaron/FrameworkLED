24ddf5df254147eca7b5abb6baaf5d3b5401feaf
Aaron Barratt <aarontbarratt@gmail.com>
2025-06-25 02:47:46 +0100

    update examples

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from LedDriver import set_brightness, LEDPaths, Brightness, Colour, set_colour
 if __name__ == "__main__":
     LEDs = LEDPaths
     # set color
-    red = Colour(red=100)
-    set_colour(LEDs.power_button_color, red)
+    green = Colour(green=100)
+    set_colour(LEDs.power_button_color, green)
     # set brightness
-    brightness = Brightness(50)
+    brightness = Brightness(69)
     set_brightness(LEDs.keyboard_backlight, brightness)
